e7a8ce687a8167370c3e0f6340ab45ad7e6cd445
kristosb <43238722+kristosb@users.noreply.github.com>
2024-01-22 18:17:09 +0100

    Update runchecks.sh

--- proj-actions/runchecks.sh
+++ proj-actions/runchecks.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-
-
-folders=("include" "includes" "src" "source" "sources" "test" "tests")
-for folder in "${folders[@]}"
-do
-    echo ${folder}
-    if [[ -d ${folder} ]]; then
-        command="cppcheck --language=c++ --error-exitcode=1 ${folder}"
-        echo ${command}
-        if ${command} | grep "error"; then
-            echo "::error ::Review your code"
-            exit 1            
-        fi
+folder = "scr"
+echo ${folder}
+if [[ -d ${folder} ]]; then
+    command="cppcheck --language=c++ --error-exitcode=1 ${folder}"
+    echo ${command}
+    if ${command} | grep "error"; then
+        echo "::error ::Review your code"
+        exit 1            
     fi
-done
+fi
+

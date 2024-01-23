b0bf3d51d3c6e81a50b9e24ff5348a3fce51451c
kristosb <43238722+kristosb@users.noreply.github.com>
2024-01-23 21:27:07 +0100

    Update runchecks.sh

--- proj-actions/runchecks.sh
+++ proj-actions/runchecks.sh
@@ -2,9 +2,11 @@
 folder = "src"
 echo ${folder}
 if [[ -d ${folder} ]]; then
-    command="cppcheck --language=c++ --error-exitcode=1 ${folder}"
+    command=$(cppcheck --language=c++ --error-exitcode=1 $mypath 2>&1)  #"cppcheck --language=c++ --error-exitcode=1 ${folder}"
     echo ${command}
-    if ${command} | grep "error"; then
+    #if ${command} | grep "error"; then
+    if [[ $command == *"error"* ]]
+    then
         echo "::error ::Review your code"
         exit 1            
     fi
